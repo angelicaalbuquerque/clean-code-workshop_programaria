5bb4232865886ace26804906b010ffd6982e1995
Angelica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-10-03 16:13:23 -0300

    SRP

--- programariaSummit23_WS_CleanCode-refactoring/srp.js
+++ programariaSummit23_WS_CleanCode-refactoring/srp.js
@@ -9,5 +9,37 @@ function calcularMediaDuracaoEpisodios(temporadaFriends) {
   
     const mediaDuracaoEpisodios = somaDuracaoTotal / numeroEpisodios;
     return mediaDuracaoEpisodios;
+  }
 }
+
+//Depois
+function assistirSerieFriends(serie, episodio) {
+  if (serie === "Friends") {
+    console.log(`Assistindo ao episódio ${episodio} de Friends.`);
+    reproduzirEpisodio(episodio);
+  } else {
+    console.log("Desculpe, esta função só funciona com Friends.");
+  }
 }
+
+function reproduzirEpisodio(episodio) {
+  console.log(`Reproduzindo o episódio ${episodio}.`);
+  // Lógica para reproduzir o episódio vai aqui...
+}
+
+// Neste exemplo, a função assistirSerieFriends tem duas 
+// responsabilidades: verificar se a série é "Friends" e, em seguida, reproduzir o episódio.
+
+function assistirEpisodioFriends(episodio) {
+  console.log(`Assistindo ao episódio ${episodio} de Friends.`);
+}
+
+function reproduzirEpisodio(episodio) {
+  console.log(`Reproduzindo o episódio ${episodio}.`);
+  // Lógica para reproduzir o episódio vai aqui...
+}
+
+// A função assistirEpisodioFriends agora lida apenas com a ação de assistir a um 
+// episódio de "Friends".
+// A função reproduzirEpisodio continua a lidar com a reprodução de um episódio,
+// como sua única responsabilidade.
