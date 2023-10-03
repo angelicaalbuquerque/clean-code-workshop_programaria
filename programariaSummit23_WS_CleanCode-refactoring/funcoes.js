140c2dacebe9827485ef43d8259cf55588eb4679
Angelica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-10-03 16:08:35 -0300

    Prática

---
+++ programariaSummit23_WS_CleanCode-refactoring/funcoes.js
@@ -0,0 +1,13 @@
+function calcularMediaEpisodiosTemporada(episodios) {
+    let somaDuracao = 0; 
+    let contador = 0;
+  
+    for (let i = 0; i < episodios.length; i++) {
+      const episodio = episodios[i];
+      somaDuracao += episodio.duracao; 
+      contador++; 
+    }
+  
+    const media = somaDuracao / contador; 
+    return media; 
+}
