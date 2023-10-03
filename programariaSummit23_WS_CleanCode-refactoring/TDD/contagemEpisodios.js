c34c966294ef81b081151e2c6ae464d1cbc2ee7f
Angelica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-10-03 16:18:46 -0300

    Usando TDD para contagem de episódios

--- programariaSummit23_WS_CleanCode-refactoring/TDD/contagemEpisodios.js
+++ programariaSummit23_WS_CleanCode-refactoring/TDD/contagemEpisodios.js
@@ -1,13 +1,12 @@
 function contarEpisodiosPorPersonagem(episodios) {
-    const contagem = {};
-    episodios.forEach((episodio) => {
-      if (!contagem[episodio.nome]) {
-        contagem[episodio.nome] = 0;
-      }
-      contagem[episodio.nome] += episodio.apareceu;
-    });
-    return contagem;
-  }
-  
-  module.exports = contarEpisodiosPorPersonagem;
-  
+  const contagemDeAparicoesPorPersonagem = {};
+  episodios.forEach((episodio) => {
+    if (!contagemDeAparicoesPorPersonagem[episodio.nome]) {
+      contagemDeAparicoesPorPersonagem[episodio.nome] = 0;
+    }
+    contagemDeAparicoesPorPersonagem[episodio.nome] += episodio.apareceu;
+  });
+  return contagemDeAparicoesPorPersonagem;
+}
+
+module.exports = contarEpisodiosPorPersonagem;
