468d5e17416f5a40a67c5592a21703cb79275ba0
Angelica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-10-03 16:09:15 -0300

    Melhorando variáveis

--- programariaSummit23_WS_CleanCode-refactoring/variaveis.js
+++ programariaSummit23_WS_CleanCode-refactoring/variaveis.js
@@ -8,3 +8,10 @@ var joeyHowYouDoin = "Frase famosa do personagem Joey";
 var centralPerkTable = "Móvel icônico do Central Perk";
 
 var rossWeWereOnABreak = "Frase longa do personagem Ross";
+
+//Depois
+const famousPhoebeSong = "Smelly Cat - Música famosa da série Friends";
+const iconicRossScene = "Pivot! - Cena icônica da série Friends";
+const famousJoeyPhrase = "How You Doin' - Frase famosa do personagem Joey";
+const iconicFriendsFurniture = "Central Perk mesa = móvel icônica do Central Perk";
+const famousRossPhrase = "We were on a break! - Frase famosa do personagem Ross";
